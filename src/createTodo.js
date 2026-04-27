4b6cbf086f9dd0f15a2eacffa07081df7e85bf0c
jshuhler <15068877+jshuhler@users.noreply.github.com>
2026-04-27 18:20:48 -0400

    add more html and css framework, start createTodo script

---
+++ src/createTodo.js
@@ -0,0 +1,17 @@
+class Todo {
+    constructor(title,description,dueDate,priority,status) {
+        if (!new.target) {
+            throw Error("You must use the 'new' operator to call the constructor.");
+        };
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.status = status;
+        this.id = crypto.randomUUID();
+    };
+};
+
+export { 
+    Todo
+};
