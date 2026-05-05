b9b5ff059edf947abdb48313c0b0344a51d69142
jshuhler <15068877+jshuhler@users.noreply.github.com>
2026-05-05 12:25:34 -0400

    feat: add dialog and event listeners for creating projects, minor dialog styling

--- src/displayControl.js
+++ src/displayControl.js
@@ -1,7 +1,12 @@
 import { todoList, createTodo } from "./createTodo.js";
+import { projectList, createProject } from "./createProject.js";
 
-// DECLARING VARIABLES 
-const dialog = document.querySelector("dialog");
+// -------------------------------
+// OPENING AND CLOSING THE NEW TODO DIALOG
+// -------------------------------
+
+// DECLARING VARIABLES FOR TODO DIALOG
+const todoDialog = document.querySelector("dialog.todo-dialog");
 const addTodoButton = document.getElementById("add-task-button");
 const closeTodoDialog = document.getElementById("close-todo-submit");
 const addTodoDialog = document.getElementById("add-todo-submit");
@@ -13,15 +18,15 @@ let todoPriority;
 
 // OPEN NEW TODO DIALOG
 addTodoButton.addEventListener('click', () => {
-    dialog.showModal();
-    console.log('you clicked the add todo item button');
+    todoDialog.showModal();
+    console.log('add todo item button press');
 });
 
 // CLOSE NEW TODO DIALOG WITHOUT ADDING ITEM
 closeTodoDialog.addEventListener('click', (e) => {
-    dialog.close();
+    todoDialog.close();
     e.preventDefault();
-    console.log('you clicked the close dialog button');
+    console.log('close todo dialog button press');
 });
 
 // CREATE TODO ITEM AND ADD TO ARRAY
@@ -31,10 +36,52 @@ addTodoDialog.addEventListener('click', (e) => {
     todoDetails = document.getElementById("todo-details").value;
     todoDueDate = document.getElementById("todo-date").value;
     todoProject = document.getElementById("todo-project").value;
-    let form = document.forms[0];
-    let radioButtons = form.elements["priority"];
+    let form = document.forms[0]; // getting the value out of the RadioNodeList value property
+    let radioButtons = form.elements["priority"]; // getting the value out of the RadioNodeList value property
     todoPriority = radioButtons.value;
     createTodo(todoTitle,todoDetails,todoDueDate,todoProject,todoPriority);
     console.log(todoList);
-    dialog.close();
+    console.log('save todo item button press');
+    todoDialog.close();
 });
+
+// -------------------------------
+// OPENING AND CLOSING THE NEW PROJECT DIALOG
+// -------------------------------
+
+// DECLARING VARIABLES FOR PROJECT DIALOG
+const projectDialog = document.querySelector("dialog.project-dialog");
+const addProjectButton = document.getElementById("add-project-button");
+const closeProjectDialog = document.querySelector(".project-dialog-close");
+const addProjectDialog = document.getElementById("add-project-submit");
+let projectName;
+
+// OPEN NEW PROJECT DIALOG
+addProjectButton.addEventListener('click', () => {
+    projectDialog.showModal();
+    console.log('add project button press');
+});
+
+// CLOSE NEW PROJECT DIALOG WITHOUT ADDING PROJECT
+closeProjectDialog.addEventListener('click', (e) => {
+    projectDialog.close();
+    e.preventDefault();
+    console.log('close project dialog button press');
+});
+
+addProjectDialog.addEventListener('click', (e) => {
+    e.preventDefault();
+    projectName = document.getElementById("project-name").value;
+    createProject(projectName);
+    console.log(projectList);
+    console.log('save project button press');
+    projectDialog.close();
+});
+
+
+
+// -------------------------------
+// CHANGING THE SORT BY PROJECT
+// -------------------------------
+
+// DECLARING VARIABLES
